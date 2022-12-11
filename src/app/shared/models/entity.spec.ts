a45a9d4a1fd6081c5c2260b444ec6dbf3be688d9
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2022-12-11 19:37:41 +0100

    Update entity.spec.ts

--- src/app/shared/models/entity.spec.ts
+++ src/app/shared/models/entity.spec.ts
@@ -1,20 +1,30 @@
 import { TestBed } from "@angular/core/testing";
-import { NgxIndexedDBService } from "ngx-indexed-db";
 import { CoreModule } from "src/app/core/core.module";
-import { EntityStateFactory } from "./entity/entity-state-factory";
+
+const MAX_TIMEOUT = 2147483647;
 
 describe("EntityFactory tests", function () {
     beforeEach(() => {
         TestBed.configureTestingModule({ imports: [CoreModule] });
     });
 
-    it("and so is a spec", async function () {
-        const indexDBService = TestBed.inject(NgxIndexedDBService);
-        const id = '1';
-        const entityApiHub: string = 'https://jsonplaceholder.typicode.com/todos';
-        const entityState$ = await EntityStateFactory.create(id, entityApiHub, indexDBService);
-        
-        //TO DO: find out good way to test those things.
-        expect(true).toBe(true);
+    // it("and so is a spec", async function () {
+    //     const indexDBService = TestBed.inject(NgxIndexedDBService);
+    //     const id = '1';
+    //     const entityApiHub: string = 'https://jsonplaceholder.typicode.com/todos';
+    //     const entityState$ = await EntityStateFactory.create(id, entityApiHub, indexDBService);
+
+    //     //TO DO: find out good way to test those things.
+    //     expect(true).toBe(true);
+    // });
+
+    it("test performance", async function () {
+        var t0 = performance.now();
+        let result = 0;
+        for (var i = Math.pow(12, 7); i >= 0; i--) {
+            result += Math.atan(i) * Math.tan(i);
+        };
+        var t1 = performance.now();
+        console.warn("Call to doSomething took " + (t1 - t0) + " milliseconds.")
     });
 });
